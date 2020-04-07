0eec765c779b350315ee0067d11b658dfbcf6c3a
taesung <ktsung94@naver.com>
2020-04-07 11:00:10 +0900

    Array

---
+++ ex13.js
@@ -0,0 +1,54 @@
+// 배열 메소드 - concat
+
+var colors = ['black', 'white', 'yellow'];
+var fruits = ['mango', 'banana', 'apple'];
+
+var test = fruits.concat(colors);
+console.log(test);
+
+// 배열 메소드 - join
+var str = fruits.join(',');
+console.log(str);
+
+// 배열 메소드 - pop, push : stack 지원
+var color = colors.pop();
+console.log(color);
+console.log(colors);
+
+colors.push('red');
+console.log(colors);
+
+// 배열 메소드 - reverse
+console.log(fruits);
+fruits.reverse();
+console.log(fruits);
+
+// 배열 메소드 - shift
+var numbers = [4000, 8000, 9000, 5000, 1000];
+console.log(numbers);
+numbers.shift();
+console.log(numbers);
+
+// 배열 메소드 - slice
+var numbers2 = numbers.slice(0, 3);
+console.log(numbers2);
+
+// 배열 메소드 - sort
+console.log(numbers);
+numbers.sort();
+console.log(numbers);
+
+// 배열 메소드 - splice
+// index 부터 count개 삭제 후 요소를 배열에 반환
+console.log(fruits);
+var fruits2 = fruits.splice(0/*index*/, 2/*count*/);
+console.log(fruits2);
+console.log(fruits);
+
+var a1 = [0,1,2,3,4];
+a1.splice(1, 3, 10);
+console.log(a1);
+
+var a2 = [0,1,2,3,4];
+a2.splice(1, 1, 10);
+console.log(a2);
